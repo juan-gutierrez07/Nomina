93157ea68e8a0f4730a067a078bf383a6d58405a
Guti <juan-gutierrez07@upc.edu.co>
2019-11-03 14:17:26 -0500

    Agregando todos los cambios de cesar

--- add_persona.php
+++ add_persona.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $direccion = $_POST['direccion'];
     $codigo_empleado = $_POST['cod-empleado'];
     $celular = $_POST['celular'];
-   // $codigo_cargo= $_POST['cargo'];
+    // $codigo_cargo= $_POST['cargo'];
     if (isset($_POST['Enviar'])) {
         if (empty($dni) || empty($nombres) || empty($apellidos) || empty($direccion) || empty($codigo_empleado) || empty($celular)) {
             echo "<script>alert('No puede dejar un campo vacio.. '); </script>";
@@ -37,48 +37,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 
-<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Agregar personas</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-        </div>
-    </div>
-    <div class="wrapper">
-        <div class="container">
-            <div class="col-md-12 section-a">
-                <div class="form-post">
-                    <form action="add_persona.php" method="POST">
-                        <label name="nombre">Nombres: </label>
-                        <input type="text" id="nombres" name="nombres" placeholder="Ingresa tus nombres" width="800px" height="200px" />
-                        <label name="apellidos">Apellidos: </label>
-                        <input type="text" id="apellidos" name="apellidos" placeholder="Ingresa tus apellidos" width="800px" height="200px" />
-                        <label name="dni">dni: </label>
-                        <input type="text" id="dni" name="dni" placeholder="Ingresa dni" width="800px" height="200px" />
-                        <label name="direccion">Direccion:</label>
-                        <input type="text" id="dirrecion" name="direccion" placeholder="Ingresa la dirrecion" width="800px" height="200px" />
-                        <label name="codempleado">Codigo empleado: </label>
-                        <input type="text" id="cod-empleado" name="cod-empleado" placeholder="Ingresa codigo " width="800px" height="200px" />
-                        <label name="celular">Celular: </label>
-                        <input type="text" id="celular" name="celular" placeholder="Ingresa celular" width="800px" height="200px" />
-                        <label name="cargos">Cargo: </label>
-                        <select name="cargo" id ="cargo">
-                            <?php
-                            $x = 1;
-                            $consulta2 = "select  * from cargo ; ";
-                            $resultado2 = mysqli_query($conexion, $consulta2);
-                            while ($datos = mysqli_fetch_array($resultado2)) {
-                                ?> <option><?php echo $datos['nombre']; ?> </option>
+<html>
 
-                            <?php        
-                            }
-                             //   $codigo_cargo = $datos['codigo'];
-                            ?>
-                        </select>
-                        <input class="enviar" type="submit" value="Enviar" name="Enviar" />
-                    </form>
-                </div>
+<head>
+    <link rel="stylesheet" href="http://127.0.0.1/Frontend-menus/alcari/css/css-agregarper.css">
+
+
+</head>
+
+<body>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Agregar personas</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
             </div>
+        </div>
+        <div class="wrapper">
+            <div class="container">
+                <div class="col-md-12 section-a">
+                    <div class="form-post">
+                        <form action="add_persona.php" method="POST">
+                            <label name="nombre">Nombres: </label>
+                            <input type="text" id="nombres" name="nombres" placeholder="Ingresa tus nombres" width="800px" height="200px" />
+                            <label name="apellidos">Apellidos: </label>
+                            <input type="text" id="apellidos" name="apellidos" placeholder="Ingresa tus apellidos" width="800px" height="200px" />
+                            <label name="dni">dni: </label>
+                            <input type="text" id="dni" name="dni" placeholder="Ingresa dni" width="800px" height="200px" />
+                            <label name="direccion">Direccion:</label>
+                            <input type="text" id="dirrecion" name="direccion" placeholder="Ingresa la dirrecion" width="800px" height="200px" />
+                            <label name="codempleado">Codigo empleado: </label>
+                            <input type="text" id="cod-empleado" name="cod-empleado" placeholder="Ingresa codigo " width="800px" height="200px" />
+                            <label name="celular">Celular: </label>
+                            <input type="text" id="celular" name="celular" placeholder="Ingresa celular" width="800px" height="200px" />
+                            <label name="cargos">Cargo: </label>
+                            <select name="cargo" id="cargo">
+                                <?php
+                                $x = 1;
+                                $consulta2 = "select  * from cargo ; ";
+                                $resultado2 = mysqli_query($conexion, $consulta2);
+                                while ($datos = mysqli_fetch_array($resultado2)) {
+                                    ?> <option><?php echo $datos['nombre']; ?> </option>
 
-            <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+                                <?php
+                                }
+                                //   $codigo_cargo = $datos['codigo'];
+                                ?>
+                            </select>
+                            <input class="enviar" type="submit" value="Enviar" name="Enviar" />
+                        </form>
+                    </div>
+                </div>
 
-</main>
+                <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+
+    </main>
+</body>
+
+</html>
--- home.php
+++ home.php
@@ -1,109 +1,34 @@
 <html>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <title>Alcari S.A.S</title>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
-</head>
+<?php include_once('template.php'); ?>
 
 <body>
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Alcari S.A.S</a>
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="/alcari/index.php">Sign out</a>
-            </li>
-        </ul>
-    </nav>
-    <hr />
-    <hr />
-    <div class="container-fluid">
-        <div class="row">
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                <div class="sidebar-sticky">
-                    <ul class="nav flex-column">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/alcari/home.php">
-                                <span data-feather="file"></span>
-                                INICIO
-                            </a>
-                        </li>
 
-                        <li class="nav-item active">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Personas
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="http://127.0.0.1/Frontend-menus/alcari/agregar-personas.php">Agregar personas</a>
-                                <a class="dropdown-item" href="http://127.0.0.1/Workspace/RegistroAsig.php">datos del trabajador</a>
-                            </div>
-                        </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://127.0.0.1/Workspace/Docentes.php">
-                                <span data-feather="file"></span>
-                                Docentes
-                            </a>
-                        </li>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">INICIO</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="container">
+                <div class="col-md-12 section-a" ss>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://127.0.0.1/Workspace/Estudiantes.php">
-                                <span data-feather="file"></span>
-                                Estudiantes
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://127.0.0.1/Workspace/Asignaturas.php">
-                                <span data-feather="file"></span>
-                                Asignaturas
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://127.0.0.1/Workspace/Grados.php">
-                                <span data-feather="file"></span>
-                                Grados
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="http://127.0.0.1/Workspace/Matriculas.php">
-                                <span data-feather="file"></span>
-                                Matricula
-                            </a>
-                        </li>
-                    </ul>
-                    </ul>
                 </div>
-            </nav>
+                <img class="d-block w-100" src="descarga.png" alt="First slide">
 
-            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">INICIO</h1>
-                    <div class="btn-toolbar mb-2 mb-md-0">
-                    </div>
-                </div>
-                <div class="wrapper">
-                    <div class="container">
-                        <div class="col-md-12 section-a" ss>
+                <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+            </div>
+        </div>
 
-                        </div>
-                        <img class="d-block w-100" src="descarga.png" alt="First slide">
 
-                        <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-
-            </main>
+    </main>
 
 </body>
 
 </html>
-
 <style>
 </style>
 
