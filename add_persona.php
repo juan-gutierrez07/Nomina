0826a394bd77afafeddd8f20939eea6419c47bf0
Guti <juan-gutierrez07@upc.edu.co>
2019-11-03 13:41:46 -0500

    Agregando datos_cargos y personas

---
+++ add_persona.php
@@ -0,0 +1,84 @@
+<?php include_once('template.php'); ?>
+
+
+
+<?php
+
+$usuario = "root";
+$contraseña = "";
+$servidor = "localhost";
+$basededatos  = "practicas";
+$conexion = mysqli_connect($servidor, $usuario, "") or die("No se ha podido conectar al servidor de Base de datos");
+$db = mysqli_select_db($conexion, $basededatos) or die("Upps! Pues va a ser que no se ha podido conectar a la base de datos");
+$consulta = "select * from personas";
+$resultado = mysqli_query($conexion, $consulta) or die("Algo ha ido mal en la consulta a la base de datos");
+//if(isset($dni) || isset($nombres) || isset($apellidos) || isset($direccion) || isset($codigo_empleado) || isset($celular)
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $dni = $_POST['dni'];
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $direccion = $_POST['direccion'];
+    $codigo_empleado = $_POST['cod-empleado'];
+    $celular = $_POST['celular'];
+   // $codigo_cargo= $_POST['cargo'];
+    if (isset($_POST['Enviar'])) {
+        if (empty($dni) || empty($nombres) || empty($apellidos) || empty($direccion) || empty($codigo_empleado) || empty($celular)) {
+            echo "<script>alert('No puede dejar un campo vacio.. '); </script>";
+        } else {
+            $datos = "INSERT INTO personas( dni,nombres,apellidos,direccion,codigo_empleado,celular)VALUES('$dni','$nombres','$apellidos','$direccion','$codigo_empleado','$celular')";
+            $resul = mysqli_query($conexion, $datos);
+            if ($resul) {
+                echo "<script>alert('Se ha agregado a la persona exitosamente '); </script>";
+            }
+        }
+    }
+}
+
+?>
+
+
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Agregar personas</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+        </div>
+    </div>
+    <div class="wrapper">
+        <div class="container">
+            <div class="col-md-12 section-a">
+                <div class="form-post">
+                    <form action="add_persona.php" method="POST">
+                        <label name="nombre">Nombres: </label>
+                        <input type="text" id="nombres" name="nombres" placeholder="Ingresa tus nombres" width="800px" height="200px" />
+                        <label name="apellidos">Apellidos: </label>
+                        <input type="text" id="apellidos" name="apellidos" placeholder="Ingresa tus apellidos" width="800px" height="200px" />
+                        <label name="dni">dni: </label>
+                        <input type="text" id="dni" name="dni" placeholder="Ingresa dni" width="800px" height="200px" />
+                        <label name="direccion">Direccion:</label>
+                        <input type="text" id="dirrecion" name="direccion" placeholder="Ingresa la dirrecion" width="800px" height="200px" />
+                        <label name="codempleado">Codigo empleado: </label>
+                        <input type="text" id="cod-empleado" name="cod-empleado" placeholder="Ingresa codigo " width="800px" height="200px" />
+                        <label name="celular">Celular: </label>
+                        <input type="text" id="celular" name="celular" placeholder="Ingresa celular" width="800px" height="200px" />
+                        <label name="cargos">Cargo: </label>
+                        <select name="cargo" id ="cargo">
+                            <?php
+                            $x = 1;
+                            $consulta2 = "select  * from cargo ; ";
+                            $resultado2 = mysqli_query($conexion, $consulta2);
+                            while ($datos = mysqli_fetch_array($resultado2)) {
+                                ?> <option><?php echo $datos['nombre']; ?> </option>
+
+                            <?php        
+                            }
+                             //   $codigo_cargo = $datos['codigo'];
+                            ?>
+                        </select>
+                        <input class="enviar" type="submit" value="Enviar" name="Enviar" />
+                    </form>
+                </div>
+            </div>
+
+            <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+
+</main>
---
+++ datos_cargos.php
@@ -0,0 +1,63 @@
+<?php include_once('template.php'); ?>
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Cargos</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+        </div>
+    </div>
+    <form id="formulario" action="datos_cargos.php" method="post">
+
+        <fieldset>
+            <label> Buscar por codigo </label> <input id="codigo" type="number" name="codigo" placeholder="codigo del cargo" />
+            <input class="button" type="submit" name="Buscar" value="Buscar" />
+        </fieldset>
+
+        <div class="wrapper">
+            <a href="datos_cargos.php"> Mostrar todos los cargos </a>
+        </div>
+    </form>
+
+
+        <?php
+
+        $usuario = "root";
+        $contraseña = "";
+        $servidor = "localhost";
+        $basededatos  = "practicas";
+
+        $conexion = mysqli_connect($servidor, $usuario, "") or die("No se ha podido conectar al servidor de Base de datos");
+        $db = mysqli_select_db($conexion, $basededatos) or die("Upps! Pues va a ser que no se ha podido conectar a la base de datos");
+        $consulta = "select * from cargo";
+        $resultado = mysqli_query($conexion, $consulta) or die("Algo ha ido mal en la consulta a la base de datos");
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $documento = 0;
+            $documento = $_POST['codigo'];
+           
+            $buscar = "select * from cargo where codigo = " . $documento;
+              if ($documento == 0 || $documento <0) {
+                    $resultado = die("No existe en la base de datos.");
+               }else{
+                    $resultado = mysqli_query($conexion, $buscar) or die("No existe en la base de datos.");
+       
+               }
+        }
+
+       
+        echo "<table border =1px >";
+        echo "<tr>";
+        echo "<th > Codigo  </th>";
+        echo "<th > Nombre </th>";
+        echo "</tr>";
+
+
+        while ($columna = mysqli_fetch_array($resultado)) {
+
+            echo "<tr>";
+            echo "<td>" . $columna['codigo']  . "   </td>  <td>" . $columna['nombre'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+       
+        ?>
+        <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
