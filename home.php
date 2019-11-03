82738cebb94bb2024189f350de868584884860aa
unknown <cesar-2007-mesa@hotmail.com>
2019-11-03 01:22:13 -0500

    datos_cargos.php

---
+++ home.php
@@ -0,0 +1,38 @@
+<html>
+<title>Alcari S.A.S</title>
+
+<?php include_once('template.php'); ?>
+
+<body>
+
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">INICIO</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="container">
+                <div class="col-md-12 section-a" ss>
+
+                </div>
+                <img class="d-block w-100" src="descarga.png" alt="First slide">
+
+                <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+            </div>
+    </div>
+
+
+    </main>
+
+</body>
+
+</html>
+
+<style>
+</style>
+
+<script>
+
+</script>
